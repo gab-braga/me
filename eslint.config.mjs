f8e50b5e352d84204f80cba74534612469cdd897
gab-braga <gabrielbraga0712@gmail.com>
2026-08-26 16:36:31 -0300

    chore: vscode config added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,12 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    rules: {
+      "react/display-name": "off",
+      "import/no-anonymous-default-export": "off",
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
